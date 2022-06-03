8c1e3414c4af082311b94764b8e573ba48fc8bef
M. Abbas Ansari <63903805+m-abbas-ansari@users.noreply.github.com>
2022-06-03 12:29:09 +0530

    Time: 214 ms (29.44%), Space: 14.8 MB (89.38%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,26 +1,20 @@
 class Solution:
         
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # iterate from 0 to n-1
-        # for a[i], do binary search for (target-a[i]) in i+1 to n-1 sub array
-        # if found => check if the found element is not same as a[i], if not great! else continue for  
-        # next i
+        # use 2 pointers: low=0, high=n-1. 
+        # if low+high > target => high_idx--
+        # if low+high < target => low_idx++
+        # else return [low_idx +1, high_idx+1]
         n = len(numbers)
-        # ret = [-1,-1]
-        for i in range(n):
-            to_find = target - numbers[i]
-            to_find_idx = -1
-            left = i+1
-            right = n-1
-            while right >= left:
-                mid = (int)((right+left)/2)
-                if numbers[mid] == to_find:
-                    to_find_idx = mid
-                    break
-                elif numbers[mid] > to_find:
-                    right = mid-1
-                else:
-                    left = mid+1
-            if to_find_idx != -1:
-                return [i+1, to_find_idx+1]
+        low = 0
+        high = n-1
+        while(low < high):
+            if numbers[low] + numbers[high] > target:
+                high -= 1
+            elif numbers[low] + numbers[high] < target:
+                low += 1
+            else:
+                return [low+1, high+1]
+        
+        
             
