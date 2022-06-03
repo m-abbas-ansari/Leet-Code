96f66e01dacb240508af64c4e6bf3689913ddd7c
M. Abbas Ansari <63903805+m-abbas-ansari@users.noreply.github.com>
2022-06-03 12:36:09 +0530

    Time: 209 ms (31.71%), Space: 15 MB (41.80%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -5,13 +5,13 @@ class Solution:
         # if low+high > target => high_idx--
         # if low+high < target => low_idx++
         # else return [low_idx +1, high_idx+1]
-        n = len(numbers)
-        low = 0
-        high = n-1
-        while(low < high):
-            if numbers[low] + numbers[high] > target:
+        low, high = 0, len(numbers) - 1
+        
+        while low < high:
+            curSum = numbers[low] + numbers[high]
+            if curSum > target:
                 high -= 1
-            elif numbers[low] + numbers[high] < target:
+            elif curSum < target:
                 low += 1
             else:
                 return [low+1, high+1]
