3d6638a7b88764cf110d8bd2b838b533457a5fa6
M. Abbas Ansari <63903805+m-abbas-ansari@users.noreply.github.com>
2022-06-05 12:39:51 +0530

    Time: 1631 ms (5.02%), Space: 27.5 MB (15.92%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        # use two pointers: left and right
+        # move the pointer which is smaller. Keep calculating vol. of water until left < right
+        
+        l, r = 0, len(height) -1
+        maxVol = 0
+        left,right = 0,0
+        while l < r:
+            curVol = min(height[r],height[l])*(r-l)
+            if curVol > maxVol:
+                maxVol = curVol
+                left,right = l,r
+                
+            if height[l] < height[r]: # move left pointer
+                l+=1
+            elif height[l] > height[r]: # move right pointer
+                r -= 1
+            else:
+                l+=1
+        #print(left,right)
+        return maxVol
