19d4c2ba7bcbbabde591b8774a510085d7e00815
M. Abbas Ansari <63903805+m-abbas-ansari@users.noreply.github.com>
2022-06-11 22:25:18 +0530

    Time: 99 ms (44.02%), Space: 18.4 MB (23.32%) - LeetHub

--- 155-min-stack/155-min-stack.py
+++ 155-min-stack/155-min-stack.py
@@ -1,31 +1,30 @@
-import sys
-class MinStack:
 
+class MinStack:
+    # Using hint 1: Consider each node in the stack having a minimum value
+    # Therefore for each value in stack, we will also store the min uptil the point that value was
+    # pushed to the stack
     def __init__(self):
         self.stack = []
         self.min = 2**31 - 1
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
         if val < self.min:
             self.min = val
-     
+        self.stack.append((val, self.min)) # appending a tuple
             
     def pop(self) -> None:
-        val = self.stack.pop()
-        if val == self.min and len(self.stack) > 0:
-            self.min = self.stack[0]
-            for i in self.stack:
-                if i < self.min:
-                    self.min = i
-        elif len(self.stack) == 0:
+        self.stack.pop()
+        if len(self.stack) != 0:
+            self.min = self.stack[-1][1]
+        else:
             self.min = 2**31 - 1
+     
 
     def top(self) -> int:
-        return self.stack[-1]
+        return self.stack[-1][0]
 
     def getMin(self) -> int:
-        return self.min
+        return self.stack[-1][1]
 
 
 # Your MinStack object will be instantiated and called as such:
