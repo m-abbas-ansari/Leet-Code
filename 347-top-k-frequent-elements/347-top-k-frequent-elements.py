f780194f2ee386b74c68b570aaf2d3637031ce1f
M. Abbas Ansari <63903805+m-abbas-ansari@users.noreply.github.com>
2022-06-19 09:15:21 +0530

    Time: 188 ms (23.93%), Space: 18.6 MB (69.82%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -6,5 +6,5 @@ class Solution:
                 freq[n] += 1
             else:
                 freq[n] = 1
-        top_freq = sorted(freq.items(), key=lambda item: item[1], reverse=True)
-        return [a[0] for a in top_freq[:k]]
+                
+        return [a[0] for a in sorted(freq.items(), key=lambda item: item[1], reverse=True)[:k]]
