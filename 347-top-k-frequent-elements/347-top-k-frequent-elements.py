896f9dfb247077dd2d9491bae26855fdecfebb01
M. Abbas Ansari <63903805+m-abbas-ansari@users.noreply.github.com>
2022-06-19 09:46:03 +0530

    Time: 160 ms (42.97%), Space: 19.6 MB (14.24%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,10 +1,23 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = {}
+        # Bucket sort: where the buckets are the counts in which numbers are put in
+        # Maintain a dictionary where keys go from 0 to n where n is the total number of 
+        # elements in the nums array. The value for each key is the corresponding element
+        # whose number of occurence in the nums array is equal to the key.
+        # To get top k: traverse dict from n to 0 (reverse order) and find top k
+        
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)] # maintains the buckets where index is the count/freq.
+        
         for n in nums:
-            if n in freq:
-                freq[n] += 1
-            else:
-                freq[n] = 1
-                
-        return [a[0] for a in sorted(freq.items(), key=lambda item: item[1], reverse=True)[:k]]
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+            
+        res = []
+        for i in range(len(nums), 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+            
