f540833d0a220bd489e14d51b3e85307e1cf7cb3
M. Abbas Ansari <63903805+m-abbas-ansari@users.noreply.github.com>
2022-06-20 09:18:37 +0530

    Time: 291 ms (65.23%), Space: 21.2 MB (55.74%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,19 +1,22 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix = []
-        suffix = []
-        pre, suf = 1, 1
-        prefix.append(pre)
-        suffix.insert(0, suf)
-        for i in range(0, len(nums)-1):
-            pre *= nums[i]
-            suf *= nums[-(i+1)]
-            prefix.append(pre)
-            suffix.insert(0, suf)
-
+        # Do 2 passes through nums array: 
+        # left -> right for prefix product 
+        # right -> left for postfix product
+        # You can just use the output array to store the resultant pre*post
+        # O(n) time, O(1) space complexity
+        
         res = []
-        for i, n in enumerate(nums):
-            res.append(int((prefix[i] * suffix[i])))
+        prefix = 1
+        for n in nums: # traversing left -> right
+            res.append(prefix)
+            prefix *= n
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1): # traversing right -> left
+            res[i] *= postfix
+            postfix *= nums[i]
         
         return res
+            
+        
         
